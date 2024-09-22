2decffcaa8af781511e62c2c64a05aabce6b9a4b
MrPowley <mrpowleyme@gmail.com>
2024-09-22 18:19:19 +0200

    added presets (rudimental)

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showerror
+import tkinter.filedialog
 import sys
 import threading
 import subprocess
+from os import getcwd
+from os.path import exists, join
+import json
+import tkinter.simpledialog
 
 from PIL import Image, ImageDraw
 import sv_ttk
@@ -11,7 +16,6 @@ import pystray
 from pystray import MenuItem as item
 
 # import logic
-
 
 class TimeSelector(tk.Frame):
     def __init__(self, parent):
@@ -24,9 +28,12 @@ class TimeSelector(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.hours_spinbox = ttk.Spinbox(self, from_=0, to=23, wrap=True, textvariable=self.hours_var, width=2, format="%02.0f")
-        self.minutes_spinbox = ttk.Spinbox(self, from_=0, to=59, wrap=True, textvariable=self.minutes_var, width=2, format="%02.0f")
-        self.seconds_spinbox = ttk.Spinbox(self, from_=0, to=59, wrap=True, textvariable=self.seconds_var, width=2, format="%02.0f")
+        self.hours_spinbox = ttk.Spinbox(
+            self, from_=0, to=23, wrap=True, textvariable=self.hours_var, width=2, format="%02.0f")
+        self.minutes_spinbox = ttk.Spinbox(
+            self, from_=0, to=59, wrap=True, textvariable=self.minutes_var, width=2, format="%02.0f")
+        self.seconds_spinbox = ttk.Spinbox(
+            self, from_=0, to=59, wrap=True, textvariable=self.seconds_var, width=2, format="%02.0f")
 
         self.hours_spinbox.grid(row=0, column=0)
         tk.Label(self, text=":").grid(row=0, column=1)
@@ -35,8 +42,18 @@ class TimeSelector(tk.Frame):
         self.seconds_spinbox.grid(row=0, column=4)
 
     def get_time(self):
-        time = int(self.hours_var.get()) * 3600 + int(self.minutes_var.get()) * 60 + int(self.seconds_var.get())
+        time = int(self.hours_var.get()) * 3600 + \
+            int(self.minutes_var.get()) * 60 + int(self.seconds_var.get())
         return time
+    
+    def get_timestamp(self):
+        return (self.hours_var.get(), self.minutes_var.get(), self.seconds_var.get())
+
+    def set_timestamp(self, h, m, s):
+        self.hours_var.set(h)
+        self.minutes_var.set(m)
+        self.seconds_var.set(s)
+
 
 class Logic:
     def __init__(self) -> None:
@@ -44,24 +61,27 @@ class Logic:
 
     def execute(self, power_signal):
         # Get abort signal
-        stopped = stop_thread.is_set() # get the value
+        stopped = stop_thread.is_set()  # get the value
         # Wait for n seconds if abort signal was issued
         for _ in range(power_signal["seconds"]):
-            stopped = stop_thread.wait(1) # Waits n seconds then returns is the event was triggered
+            # Waits n seconds then returns is the event was triggered
+            stopped = stop_thread.wait(1)
             if stopped:
                 break
-        
+
         if not stopped:
             # Start the pre-ext command
-            thread = threading.Thread(target=execute_custom_command, args=(power_signal['command'],power_signal['text_catch']))
+            thread = threading.Thread(target=execute_custom_command, args=(
+                power_signal['command'], power_signal['text_catch']))
             thread.start()
             thread.join()
-        
+
         # Get abort signal after pre-ext command ended
         stopped = stop_thread.is_set()
         if not stopped:
             # Send the extinction signal
-            subprocess.Popen(power_signal['signal'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.Popen(
+                power_signal['signal'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         else:
             print("Canceled")
 
@@ -72,7 +92,8 @@ class Logic:
                         "command": command_var.get(),
                         "text_catch": text_catch_var.get()}
         # Execute the desired actions
-        self.thread = threading.Thread(target=logic.execute, args=(power_signal,) )
+        self.thread = threading.Thread(
+            target=logic.execute, args=(power_signal,))
         self.thread.start()
 
     def shutdown(self):
@@ -82,7 +103,8 @@ class Logic:
                         "command": command_var.get(),
                         "text_catch": text_catch_var.get()}
         # Execute the desired actions
-        self.thread = threading.Thread(target=logic.execute, args=(power_signal,))
+        self.thread = threading.Thread(
+            target=logic.execute, args=(power_signal,))
         self.thread.start()
 
     def reboot(self):
@@ -92,15 +114,17 @@ class Logic:
                         "command": command_var.get(),
                         "text_catch": text_catch_var.get()}
         # Execute the desired actions
-        self.thread = threading.Thread(target=logic.execute, args=(power_signal,))
+        self.thread = threading.Thread(
+            target=logic.execute, args=(power_signal,))
         self.thread.start()
         print("Rebooted")
-        
+
     def stop(self):
         # Set abort signal
         stop_thread.set()
         # Wait for threads to end
         self.thread.join()
+
 
 def move_window_to_bottom_right(window):
     window.update_idletasks()
@@ -119,6 +143,7 @@ def move_window_to_bottom_right(window):
     # Déplacer la fenêtre
     window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+
 def create_image():
     # Crée une image de 16x16 pixels pour l'icône du tray
     width = 16
@@ -128,13 +153,16 @@ def create_image():
     dc.rectangle((0, 0, width, height))
     return image
 
+
 def quit_window(icon):
     icon.stop()
     root.destroy()
 
+
 def show_window(icon):
     icon.stop()
     root.after(0, root.deiconify)
+
 
 def hide_window(*args):
     root.withdraw()
@@ -143,9 +171,11 @@ def hide_window(*args):
     icon = pystray.Icon("icon", image, "Timed Power State", menu)
     icon.run()
 
+
 def execute_custom_command(command: str, text_catch: str) -> None:
     # Starting pre-ext command
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
 
     # While not aborted
     while not stop_thread.is_set():
@@ -161,10 +191,58 @@ def execute_custom_command(command: str, text_catch: str) -> None:
             # If user set a text_catch, look for it in the output
             if text_catch and text_catch in line_output:
                 break
-    
+
     # End process if aborted
     process.terminate()
     return
+
+def choose_preset(preset):
+    h, m, s = preset["hh"], preset["mm"], preset["ss"]
+    time_selector.set_timestamp(h, m, s)
+
+    command = preset["pre-extinction_command"]
+    command_var.set(command)
+
+    text_catch = preset["text_catch"]
+    text_catch_var.set(text_catch)
+
+    if "auto_hide_to_tray" in preset and preset["auto_hide_to_tray"]:
+        hide_window()
+
+def load_presets(show_presets_bool) -> dict:
+    # preset_file_path = tkinter.filedialog.askopenfilename(title="Ouvrir un fichier",filetypes=[('Json file','.json'),('all files','.*')])
+    preset_file_path = join(PWD, "presets.json")
+    if preset_file_path and exists(preset_file_path):
+        with open(preset_file_path, "r") as f:
+            presets = json.load(f)
+            if show_presets_bool:
+                show_presets(presets)
+            return presets
+    else:
+        return None
+        
+def show_presets(presets):
+    for preset in presets:
+        sub_menu_presets.add_command(label=preset, command=lambda: choose_preset(presets[preset]))
+
+def save_preset():
+    name = tkinter.simpledialog.askstring(title="Save preset", prompt="Choose a name for your new preset")
+    h, m, s = time_selector.get_timestamp()
+    command = command_var.get()
+    text_catch = text_catch_var.get()
+    presets = load_presets(False)
+    preset = {}
+    preset["hh"], preset["mm"], preset["ss"] = h, m, s
+    preset["pre-extinction_command"] = command
+    preset["text_catch"] = text_catch
+    presets[name] = preset
+
+    preset_file_path = join(PWD, "presets.json")
+    with open(preset_file_path, "w") as f:
+        json.dump(presets, f, indent=4)
+
+
+PWD = getcwd()
 
 stop_thread = threading.Event()
 logic = Logic()
@@ -173,6 +251,19 @@ root = tk.Tk()
 root.title("Timed Power State")
 
 root.after(0, lambda: move_window_to_bottom_right(root))
+
+menubar = tk.Menu(root)
+
+menu_presets = tk.Menu(menubar, tearoff=0)
+menu_presets.add_command(label="Charger les presets", command=lambda: load_presets(True))
+menu_presets.add_command(label="Sauvegarder le preset", command=save_preset)
+menubar.add_cascade(label="Presets", menu=menu_presets)
+
+sub_menu_presets = tk.Menu(menubar, tearoff=0)
+menu_presets.add_cascade(label="Presets", menu=sub_menu_presets)
+
+root.config(menu=menubar)
+
 
 time_selector = TimeSelector(root)
 time_selector.pack(padx=10, pady=10)
@@ -183,7 +274,7 @@ command_frame.pack(padx=10, pady=10)
 
 command_text = ttk.Label(
     command_frame,
-    text="Pre-extinction command "  
+    text="Pre-extinction command "
 )
 command_text.pack(side="left")
 
@@ -217,7 +308,7 @@ button_frame.pack(expand=True)
 hibernate_button = ttk.Button(
     button_frame,
     text="Hibernate",
-    command= logic.hibernate
+    command=logic.hibernate
     # command = lambda: print(str(command_var.get()))
 )
 hibernate_button.pack(side=tk.LEFT, padx=5)
@@ -225,14 +316,14 @@ hibernate_button.pack(side=tk.LEFT, padx=5)
 shutdown_button = ttk.Button(
     button_frame,
     text="Shutdown",
-    command= lambda: logic.shutdown(time_selector.get_time(), command_var.get())
+    command=lambda: logic.shutdown(time_selector.get_time(), command_var.get())
 )
 shutdown_button.pack(side=tk.LEFT, padx=5)
 
 reboot_button = ttk.Button(
     button_frame,
     text="Reboot",
-    command= lambda: logic.reboot(time_selector.get_time(), command_var.get())
+    command=lambda: logic.reboot(time_selector.get_time(), command_var.get())
 )
 reboot_button.pack(side=tk.LEFT, padx=5)
 
